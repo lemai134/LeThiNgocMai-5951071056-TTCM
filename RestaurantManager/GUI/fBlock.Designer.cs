cc63b241f223d550e2d82584aefeba933431d8ba
lemai134 <lethingocmai134@gmail.com>
2021-06-15 15:08:18 +0700

    Complete

---
+++ RestaurantManager/GUI/fBlock.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace RestaurantManager.GUI
+{
+    partial class fBlock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fBlock));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = global::RestaurantManager.Properties.Resources.KhungMo1;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.txtPass);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.ForeColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(747, 225);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(577, 375);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPass.Location = new System.Drawing.Point(239, 178);
+            this.txtPass.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(307, 41);
+            this.txtPass.TabIndex = 2;
+            this.txtPass.UseSystemPasswordChar = true;
+            this.txtPass.TextChanged += new System.EventHandler(this.txtPass_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 178);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(223, 36);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Nhập mật khẩu:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(138, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(336, 58);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Khóa bảo mật";
+            // 
+            // fBlock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1924, 1084);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "fBlock";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.fBlock_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ RestaurantManager/GUI/fNextTable.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace RestaurantManager.GUI
+{
+    partial class fNextTable
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fNextTable));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbBan = new System.Windows.Forms.Label();
+            this.cbToTalbe = new System.Windows.Forms.ComboBox();
+            this.btnChuyen = new System.Windows.Forms.Button();
+            this.dgvHoaDon = new System.Windows.Forms.DataGridView();
+            this.clSTT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clTenMon = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clSoLuong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clDonGia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clThanhTien = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHoaDon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Từ bàn:";
+            // 
+            // lbBan
+            // 
+            this.lbBan.BackColor = System.Drawing.Color.White;
+            this.lbBan.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbBan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbBan.ForeColor = System.Drawing.Color.Red;
+            this.lbBan.Location = new System.Drawing.Point(102, 33);
+            this.lbBan.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbBan.Name = "lbBan";
+            this.lbBan.Size = new System.Drawing.Size(181, 28);
+            this.lbBan.TabIndex = 1;
+            this.lbBan.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // cbToTalbe
+            // 
+            this.cbToTalbe.BackColor = System.Drawing.Color.White;
+            this.cbToTalbe.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbToTalbe.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbToTalbe.FormattingEnabled = true;
+            this.cbToTalbe.Location = new System.Drawing.Point(114, 326);
+            this.cbToTalbe.Margin = new System.Windows.Forms.Padding(4);
+            this.cbToTalbe.Name = "cbToTalbe";
+            this.cbToTalbe.Size = new System.Drawing.Size(146, 37);
+            this.cbToTalbe.TabIndex = 2;
+            // 
+            // btnChuyen
+            // 
+            this.btnChuyen.BackColor = System.Drawing.Color.Transparent;
+            this.btnChuyen.BackgroundImage = global::RestaurantManager.Properties.Resources.Next;
+            this.btnChuyen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnChuyen.FlatAppearance.BorderSize = 0;
+            this.btnChuyen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnChuyen.Location = new System.Drawing.Point(467, 318);
+            this.btnChuyen.Margin = new System.Windows.Forms.Padding(4);
+            this.btnChuyen.Name = "btnChuyen";
+            this.btnChuyen.Size = new System.Drawing.Size(70, 43);
+            this.btnChuyen.TabIndex = 3;
+            this.btnChuyen.UseVisualStyleBackColor = false;
+            this.btnChuyen.Click += new System.EventHandler(this.btnChuyen_Click);
+            // 
+            // dgvHoaDon
+            // 
+            this.dgvHoaDon.AllowUserToAddRows = false;
+            this.dgvHoaDon.BackgroundColor = System.Drawing.Color.White;
+            this.dgvHoaDon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHoaDon.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clSTT,
+            this.clTenMon,
+            this.clSoLuong,
+            this.clDonGia,
+            this.clThanhTien});
+            this.dgvHoaDon.Location = new System.Drawing.Point(17, 83);
+            this.dgvHoaDon.Name = "dgvHoaDon";
+            this.dgvHoaDon.ReadOnly = true;
+            this.dgvHoaDon.RowHeadersVisible = false;
+            this.dgvHoaDon.RowHeadersWidth = 62;
+            this.dgvHoaDon.Size = new System.Drawing.Size(497, 231);
+            this.dgvHoaDon.TabIndex = 10;
+            // 
+            // clSTT
+            // 
+            this.clSTT.HeaderText = "STT";
+            this.clSTT.MinimumWidth = 8;
+            this.clSTT.Name = "clSTT";
+            this.clSTT.ReadOnly = true;
+            this.clSTT.Width = 50;
+            // 
+            // clTenMon
+            // 
+            this.clTenMon.DataPropertyName = "TenMonAn";
+            this.clTenMon.HeaderText = "Tên";
+            this.clTenMon.MinimumWidth = 8;
+            this.clTenMon.Name = "clTenMon";
+            this.clTenMon.ReadOnly = true;
+            this.clTenMon.Width = 150;
+            // 
+            // clSoLuong
+            // 
+            this.clSoLuong.DataPropertyName = "SoLuong";
+            this.clSoLuong.HeaderText = "Số lượng";
+            this.clSoLuong.MinimumWidth = 8;
+            this.clSoLuong.Name = "clSoLuong";
+            this.clSoLuong.ReadOnly = true;
+            this.clSoLuong.Width = 90;
+            // 
+            // clDonGia
+            // 
+            this.clDonGia.DataPropertyName = "DonGia";
+            this.clDonGia.HeaderText = "Đơn Giá";
+            this.clDonGia.MinimumWidth = 8;
+            this.clDonGia.Name = "clDonGia";
+            this.clDonGia.ReadOnly = true;
+            this.clDonGia.Width = 150;
+            // 
+            // clThanhTien
+            // 
+            this.clThanhTien.DataPropertyName = "ThanhTien";
+            this.clThanhTien.HeaderText = "Thành tiền";
+            this.clThanhTien.MinimumWidth = 8;
+            this.clThanhTien.Name = "clThanhTien";
+            this.clThanhTien.ReadOnly = true;
+            this.clThanhTien.Width = 150;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 332);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Đến bàn:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(288, 338);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Xác nhận chuyển:";
+            // 
+            // fNextTable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Snow;
+            this.ClientSize = new System.Drawing.Size(565, 384);
+            this.Controls.Add(this.dgvHoaDon);
+            this.Controls.Add(this.btnChuyen);
+            this.Controls.Add(this.cbToTalbe);
+            this.Controls.Add(this.lbBan);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "fNextTable";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Chuyển bàn";
+            this.Load += new System.EventHandler(this.fNextTable_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHoaDon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbBan;
+        private System.Windows.Forms.ComboBox cbToTalbe;
+        private System.Windows.Forms.Button btnChuyen;
+        private System.Windows.Forms.DataGridView dgvHoaDon;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clSTT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clTenMon;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clSoLuong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clDonGia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clThanhTien;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
