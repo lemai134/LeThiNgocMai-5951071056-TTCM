9c5ab01f397c3f02de792edbda21df544806aa51
lemai134 <lethingocmai134@gmail.com>
2021-07-02 15:26:52 +0700

    commit

--- RestaurantManager/fgioithieu.Designer.cs
+++ RestaurantManager/fgioithieu.Designer.cs
@@ -39,7 +39,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Crimson;
-            this.label1.Location = new System.Drawing.Point(218, 9);
+            this.label1.Location = new System.Drawing.Point(272, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(475, 58);
             this.label1.TabIndex = 0;
